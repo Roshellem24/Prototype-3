0c385470e76b04ea49d2895aa723b582793431a0
Roshellem24 <98101425+Roshellem24@users.noreply.github.com>
2022-11-16 11:50:09 -0500

    3.2

--- Assets/Scripts/RepeatBackground.cs
+++ Assets/Scripts/RepeatBackground.cs
@@ -1,7 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float jumpForce;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    public bool gameOver = false;
 
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+
+        
+    }
+
+    private void OnCollisionEnter (Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground")){
+            isOnGround = true;
+
+        } else if (collision.gameObject.CompareTag("Obstacle")){
+            gameOver = true;
+            Debug.Log("Game Over");
+        }
+    }
+}
 public class RepeatBackground : MonoBehaviour
 {
     private Vector3 startPos;
@@ -9,14 +44,14 @@ public class RepeatBackground : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        startPos = transform.position; 
-        repeatWidth = GetComponent<BoxCollider>()size.x / 2;
+        startPos = transform.position;
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < startPos.x repeatWidth) 
+        if (transform.position.x < startPos.x - repeatWidth)
         {
             transform.position = startPos;
         }
