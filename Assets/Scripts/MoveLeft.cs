6274c4bbcc1a1454737bb1b14cc9ad094784fd82
Roshellem24 <98101425+Roshellem24@users.noreply.github.com>
2022-11-16 12:01:45 -0500

    3.2 fix

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -1,4 +1,8 @@
-﻿public class MoveLeft : MonoBehaviour
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
 {
   private float speed = 30;
     private PlayerController playerControllerScript;
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,4 +1,9 @@
-﻿public class SpawnManager : MonoBehaviour
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
